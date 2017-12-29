ab12e98fc30acd2706e1edda65af722dfeda0314
Nguyen Quang Minh <nquangminh9x@gmail.com>
2017-12-29 21:37:29 +0700

    last commit

--- Example/Example/decision.js
+++ Example/Example/decision.js
@@ -1,21 +1,7 @@
 // return false to cheat, true to cooperate
 const decide = (history = []) => {
-  
-  return history.length==0 ? true : coOperate(history[history.length - 1].theirs); 
-  // const IfIChooseTrue = history.filter(historyDecide => historyDecide.yours == true);
-  // const IfIChooseFalse = history.filter(historyDecide =>historyDecide.yours == false);
-  // const interestRateIfIChooseTrue = IfIChooseTrue.map(historyDecideIChooseTrue => totalMoney(historyDecideIChooseTrue.yours , historyDecideIChooseTrue.theirs));
-  // const interestRateIfIChooseFalse = IfIChooseFalse.map(historyDecideIChooseFalse => totalMoney(historyDecideIChooseFalse.yours , historyDecideIChooseFalse.theirs));
-  // const totalMoneyIfIChooseTrue = interestRateIfIChooseTrue.reduce((sum , number) => number+sum , 0);
-  // const totalMoneyIfIChooseFalse = interestRateIfIChooseFalse.reduce((summ , numberr) => numberr+summ , 0);
-  // return totalMoneyIfIChooseFalse >= totalMoneyIfIChooseTrue ? false : true;
-  
-}
-// function totalMoney(me ,  you){
- 
-//  return me == false ? (you==true ? 500+ 3 : 500):(you==true ? 500+ 2 :  500-1);
-  
-// }
+    return history.length==0 ? true : coOperate(history[history.length - 1].theirs); 
+ }
  function coOperate(enemyLastChoose){
    return enemyLastChoose == true ? true :((Math.floor(Math.random()*30))==1 ? true : false);
  }
