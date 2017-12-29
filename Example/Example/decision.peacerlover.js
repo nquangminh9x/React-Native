f40da9675df1008105d3a6ba5db7004c8e1ab429
Nguyen Quang Minh <nquangminh9x@gmail.com>
2017-12-29 20:21:14 +0700

    Decision

--- Example/Example/decision.peacerlover.js
+++
@@ -1,6 +0,0 @@
-// return false to cheat, true to cooperate
-const decide = (history = []) => {
-  return false;
-}
-
-module.exports = decide;
---
+++ Example/decision.js
@@ -0,0 +1,21 @@
+// return false to cheat, true to cooperate
+const decide = (history = []) => {
+  
+  const IfIChooseTrue = history.filter(historyDecide ,historyDecide.yours == true);
+  const IfIChooseFalse = history.filter(historyDecide ,historyDecide.yours == false);
+  const interestRateIfIChooseTrue = IfIChooseTrue.map(historyDecideIChooseTrue , totalMoney(historyDecideIChooseTrue.yours , historyDecideIChooseTrue.theirs));
+  const interestRateIfIChooseFalse = IfIChooseFalse.map(historyDecideIChooseFalse , totalMoney(historyDecideIChooseFalse.yours , historyDecideIChooseFalse.theirs));
+  const totalMoneyIfIChooseTrue = interestRateIfIChooseTrue.reduce((sum , number) => number+sum , 0);
+  const totalMoneyIfIChooseFalse = interestRateIfIChooseFalse.reduce((summ , numberr) => numberr+summ , 0);
+  return totalMoneyIfIChooseFalse > totalMoneyIfIChooseTrue ? false : true;
+  
+}
+function totalMoney(me ,  you){
+ 
+ return me == false ? (you==true ? 500+ 3 : 500):(you==true ? 500+ 2 :  500-1);
+  
+}
+
+  
+module.exports = decide; 
+
