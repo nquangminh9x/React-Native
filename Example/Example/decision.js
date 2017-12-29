4e85aa49a088e7817cae30bd4e6c03cdbca08b06
Nguyen Quang Minh <nquangminh9x@gmail.com>
2017-12-29 21:43:03 +0700

    last of last

--- Example/Example/decision.js
+++ Example/Example/decision.js
@@ -3,7 +3,7 @@ const decide = (history = []) => {
     return history.length==0 ? true : coOperate(history[history.length - 1].theirs); 
  }
  function coOperate(enemyLastChoose){
-   return enemyLastChoose == true ? true :((Math.floor(Math.random()*30))==1 ? true : false);
+   return enemyLastChoose == true ? true :((Math.floor(Math.random()*20))==1 ? true : false);
  }
 module.exports = decide; 
 
